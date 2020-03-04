098b9d426b7f4fc2fecc22699a596732ab8f8e01
guoliduo521 <57818788+guoliduo521@users.noreply.github.com>
2020-03-04 13:03:08 +0800

    Update request.js

--- src/network/request.js
+++ src/network/request.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export  function request(config){
   const instance = axios.create({
-    baseURL:"http://106.54.54.237:8000/api/v1",
+    baseURL:"http://106.54.54.237:8000/api/wh",
     timeout:5000,
   })
   return instance(config)
